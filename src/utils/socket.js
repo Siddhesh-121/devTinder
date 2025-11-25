a0132ba190f25afd062c97af91f7fd8022f99e94
siddhesh <siddheshkamthe12@gmail.com>
2025-11-25 12:44:38 +0000

    Real-time Chat feature added

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,15 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  const roomId = crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("$"))
+    .digest("hex");
+
+  return roomId;
+};
+
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
@@ -7,9 +18,17 @@ const initializeSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", () => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      socket.join(roomId);
+      console.log(firstName + " joined " + roomId);
+    });
 
-    socket.on("sendMessage", () => {});
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " says " + text);
+      io.to(roomId).emit("recieveMessage", { firstName, targetUserId, text });
+    });
 
     socket.on("onDisconnect", () => {});
   });
